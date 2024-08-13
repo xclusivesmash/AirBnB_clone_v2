31882bdb857b7f445c8e5e6b66d4d037bbe539d7
xclusivesmash <sbumatshiane916@gmail.com>
2024-08-13 12:06:30 -0700

    Initial commit

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Module: 3-python_route
+Description: using more routes.
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Returns a simple message"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_world():
+    """Extention of the firts route."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_text(text):
+    """Displays the given text."""
+    string = f"C {text}"
+    return string.replace("_", " ")
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_print(text="is cool"):
+    """Prints a text based on input."""
+    string = f"Python {text}"
+    return string.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
